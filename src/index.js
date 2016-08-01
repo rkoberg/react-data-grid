35bbcbf70f5fced33555aa252c320b5fe01c55c3
rkoberg <rob.koberg@gmail.com>
2016-08-01 09:26:32 -0700

    fix for server-side render

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+
+const canUseDOM = !!(
+  (typeof window !== 'undefined' &&
+  window.document && window.document.createElement)
+);
+
+if (!canUseDOM)
+  window = {
+    addEventListener: function addEventListener(){},
+    attachEvent: function attachEvent(){},
+    innerHeight: 0,
+    innerWidth: 0,
+    removeEventListener: function attachEvent(){},
+    event: null,
+  };
+
 const Grid = require('./ReactDataGrid');
 const Row  = require('./Row');
 const Cell = require('./Cell');
